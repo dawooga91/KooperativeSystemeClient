97e775bf28e5ba9f2f874f325595efe73896f833
cody66 <cody66@172.22.211.11>
2017-07-12 12:30:53 +0200

    AdminView Update Design

--- DYUClient/src/main/java/de/fhdortmund/koopSys/DYUServer/ui/View/AdminView.java
+++ DYUClient/src/main/java/de/fhdortmund/koopSys/DYUServer/ui/View/AdminView.java
@@ -12,6 +12,7 @@ import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
@@ -36,6 +37,17 @@ public class AdminView extends VerticalLayout implements View {
 	private Button btnDeleteQuestion;
 	private Button btnDelete;
 	private TextField question;
+	private Label questionLabel;
+	private Label userCountLabel;
+	private Label yesLabel;
+	private Label noLabel;
+	private Label prozentLabel;
+	
+	private String Frage = "Noch keine Frage gestellt!";
+	private int userCount = 0;
+	private int yes  = 0 ;
+	private int no = 0;
+	private int prozent  = 0;
 			
 	@PostConstruct
 	private void _init() {
@@ -43,13 +55,13 @@ public class AdminView extends VerticalLayout implements View {
 		
 		VerticalLayout verticalLayout = new VerticalLayout();
 		
-		// Textfield Username
+		// Textfield Frage
 				question = new TextField("Frage");
 				question.setIcon(FontAwesome.QUESTION);
 				question.focus();
 				verticalLayout.addComponent(question);
 				
-		// Button		
+		// Button	Frage stellen & Frage schließen
 				HorizontalLayout buttonLayout = new HorizontalLayout();
 				btnQuestion = new Button("Frage stellen");
 				btnDeleteQuestion = new Button("Frage schließen", FontAwesome.TIMES);
@@ -57,6 +69,37 @@ public class AdminView extends VerticalLayout implements View {
 				buttonLayout.addComponent(btnDeleteQuestion);
 				verticalLayout.addComponent(buttonLayout);
 				
+		//Anzeige
+				VerticalLayout lectureLayout = new VerticalLayout();
+				questionLabel = new Label(Frage);
+				
+				HorizontalLayout thumps = new HorizontalLayout();
+				yesLabel = new Label("" + yes);
+				yesLabel.setIcon(FontAwesome.THUMBS_UP);
+				noLabel = new Label("" + no);
+				noLabel.setIcon(FontAwesome.THUMBS_DOWN);
+				userCountLabel = new Label("" + userCount);
+				userCountLabel.setIcon(FontAwesome.USERS);
+				thumps.addComponent(yesLabel);
+				thumps.addComponent(noLabel);
+				thumps.addComponent(userCountLabel);
+				
+		//Ja%
+				if(userCount==0  || (yes==0 && no==0)) {
+					prozentLabel = new Label("Es hat noch niemand an der Abstimmung teilgenommen!");
+					}
+				else {
+					prozent = yes/userCount*100;
+					prozentLabel = new Label("Es haben " + prozent +"% mit JA abgestimmt!");
+				}
+				
+				lectureLayout.addComponent(questionLabel);		
+				lectureLayout.addComponent(thumps);
+				lectureLayout.addComponent(prozentLabel);
+				
+				verticalLayout.addComponent(lectureLayout);
+				
+		// Button	Vorlesung löschen	
 				btnDelete = new Button("Vorlesung löschen", FontAwesome.TRASH);	
 				verticalLayout.addComponent(btnDelete);
 				
