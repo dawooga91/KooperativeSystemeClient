252e1d9930916f18360a566a93cee4fc261cfaf0
cody66 <cody66@172.22.211.11>
2017-07-12 11:23:41 +0200

    AdminView Design v0.1

--- DYUClient/src/main/java/de/fhdortmund/koopSys/DYUServer/ui/View/AdminView.java
+++ DYUClient/src/main/java/de/fhdortmund/koopSys/DYUServer/ui/View/AdminView.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 import de.fhdortmund.koopSys.DYUServer.ui.listener.AdminListener;
@@ -25,16 +30,45 @@ public class AdminView extends VerticalLayout implements View {
 	private static final long serialVersionUID = 1L;
 	public static final String NAME = "de.fhdortmund.koopSys.DYUServer.ui.View.AdminView";
 
+	
+	// Components
+	private Button btnQuestion;
+	private Button btnDeleteQuestion;
+	private Button btnDelete;
+	private TextField question;
+			
 	@PostConstruct
 	private void _init() {
 		setSizeFull();
 		
-		HorizontalLayout horizontalLayout = new HorizontalLayout();
+		VerticalLayout verticalLayout = new VerticalLayout();
 		
-		setCaption("Vorlesung");
-		
-		
-		
+		// Textfield Username
+				question = new TextField("Frage");
+				question.setIcon(FontAwesome.QUESTION);
+				question.focus();
+				verticalLayout.addComponent(question);
+				
+		// Button		
+				HorizontalLayout buttonLayout = new HorizontalLayout();
+				btnQuestion = new Button("Frage stellen");
+				btnDeleteQuestion = new Button("Frage schließen", FontAwesome.TIMES);
+				buttonLayout.addComponent(btnQuestion);
+				buttonLayout.addComponent(btnDeleteQuestion);
+				verticalLayout.addComponent(buttonLayout);
+				
+				btnDelete = new Button("Vorlesung löschen", FontAwesome.TRASH);	
+				verticalLayout.addComponent(btnDelete);
+				
+				
+		// lobbyPanel
+				Panel lobbyPanel = new Panel("Name der Vorlesung");
+				lobbyPanel.setWidthUndefined();
+				lobbyPanel.setContent(verticalLayout);
+				addComponent(lobbyPanel);
+				setComponentAlignment(lobbyPanel, Alignment.MIDDLE_CENTER);
+				
+		//setCaption("");
 		
 		
 		
